20c4d6f76d1bac59b96598e947d1839e9db5294f
Zaidi2126 <shehroz.i.zaidi@gmail.com>
2025-10-17 02:46:04 +0400

    deployment

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,44 @@
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useFetcher,
+  useLoaderData,
+  useNavigation,
+  useRouteError,
 } from '@remix-run/react';
-import { json } from '@remix-run/node';
-import { createCookieSessionStorage } from '@remix-run/node';
+import { createCookieSessionStorage, json } from '@remix-run/node';
+import { ThemeProvider, themeStyles } from '~/components/theme-provider';
+import GothamBook from '~/assets/fonts/gotham-book.woff2';
+import GothamMedium from '~/assets/fonts/gotham-medium.woff2';
 import logoJdm from '~/assets/logojdm.png';
-import config from '~/config.json';
-import { ThemeProvider } from '~/components/theme-provider';
+import { useEffect } from 'react';
+import { Error } from '~/layouts/error';
 import { VisuallyHidden } from '~/components/visually-hidden';
 import { Navbar } from '~/layouts/navbar';
-import { Footer } from '~/components/footer';
-import { useLoaderData } from '@remix-run/react';
-import { useFetcher } from '@remix-run/react';
+import { Progress } from '~/components/progress';
+import config from '~/config.json';
+import styles from './root.module.css';
+import './reset.module.css';
+import './global.module.css';
 
 export const links = () => [
-  { rel: 'preload', href: '/fonts/ipa-gothic.woff2', as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' },
-  { rel: 'preload', href: '/fonts/gotham-book.woff2', as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' },
-  { rel: 'preload', href: '/fonts/gotham-medium.woff2', as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' },
-  { rel: 'preload', href: '/fonts/gotham-bold.woff2', as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' },
-  { rel: 'preload', href: '/fonts/gotham-book-italic.woff2', as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' },
-  { rel: 'preload', href: '/fonts/gotham-medium-italic.woff2', as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' },
-  { rel: 'preload', href: '/fonts/gotham-bold-italic.woff2', as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' },
-  { rel: 'preload', href: '/fonts/katakana.woff2', as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' },
+  {
+    rel: 'preload',
+    href: GothamMedium,
+    as: 'font',
+    type: 'font/woff2',
+    crossOrigin: '',
+  },
+  {
+    rel: 'preload',
+    href: GothamBook,
+    as: 'font',
+    type: 'font/woff2',
+    crossOrigin: '',
+  },
   { rel: 'manifest', href: '/manifest.json' },
   { rel: 'icon', href: logoJdm, type: 'image/png' },
   { rel: 'shortcut_icon', href: '/shortcut.png', type: 'image/png', sizes: '64x64' },
@@ -34,6 +47,11 @@ export const links = () => [
 ];
 
 export const loader = async ({ request }) => {
+  const { url } = request;
+  const { pathname } = new URL(url);
+  const pathnameSliced = pathname.endsWith('/') ? pathname.slice(0, -1) : url;
+  const canonicalUrl = `${config.url}${pathnameSliced}`;
+
   const { getSession, commitSession } = createCookieSessionStorage({
     cookie: {
       name: '__session',
@@ -50,7 +68,7 @@ export const loader = async ({ request }) => {
   const theme = session.get('theme') || 'dark';
 
   return json(
-    { theme },
+    { canonicalUrl, theme },
     {
       headers: {
         'Set-Cookie': await commitSession(session),
@@ -60,8 +78,9 @@ export const loader = async ({ request }) => {
 };
 
 export default function App() {
-  let { theme } = useLoaderData();
+  let { canonicalUrl, theme } = useLoaderData();
   const fetcher = useFetcher();
+  const { state } = useNavigation();
 
   if (fetcher.formData?.has('theme')) {
     theme = fetcher.formData.get('theme');
@@ -74,28 +93,70 @@ export default function App() {
     );
   }
 
+  useEffect(() => {
+    console.info(
+      `${config.ascii}\n`,
+      `Taking a peek huh? Check out the source code: ${config.repo}\n\n`
+    );
+  }, []);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Theme color doesn't support oklch so I'm hard coding these hexes for now */}
+        <meta name="theme-color" content={theme === 'dark' ? '#111' : '#F2F2F2'} />
+        <meta
+          name="color-scheme"
+          content={theme === 'light' ? 'light dark' : 'dark light'}
+        />
+        <style dangerouslySetInnerHTML={{ __html: themeStyles }} />
         <Meta />
         <Links />
+        <link rel="canonical" href={canonicalUrl} />
       </head>
-      <body>
+      <body data-theme={theme}>
         <ThemeProvider theme={theme} toggleTheme={toggleTheme}>
-          <VisuallyHidden>
-            <a href="#main">Skip to main content</a>
+          <Progress />
+          <VisuallyHidden showOnFocus as="a" className={styles.skip} href="#main-content">
+            Skip to main content
           </VisuallyHidden>
           <Navbar />
-          <main id="main">
+          <main
+            id="main-content"
+            className={styles.container}
+            tabIndex={-1}
+            data-loading={state === 'loading'}
+          >
             <Outlet />
           </main>
-          <Footer />
         </ThemeProvider>
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#111" />
+        <meta name="color-scheme" content="dark light" />
+        <style dangerouslySetInnerHTML={{ __html: themeStyles }} />
+        <Meta />
+        <Links />
+      </head>
+      <body data-theme="dark">
+        <Error error={error} />
+        <ScrollRestoration />
+        <Scripts />
       </body>
     </html>
   );
